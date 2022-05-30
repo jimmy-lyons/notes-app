5a777c90c56bd9d9aeac18bd235d02b807e42f4e
Jimmy Lyons <jimmylyons1@hotmail.co.uk>
2022-05-30 15:15:26 +0100

    add test to reset list

--- notesModel.test.js
+++ notesModel.test.js
@@ -12,4 +12,12 @@ describe("Notes", () => {
 
     expect(notes.getNotes()).toEqual(["Buy vanilla"]);
   })
+
+  it("Empties list array when reset", () => {
+    const notes = new notesModel;
+    notes.addNote("Buy vanilla");
+    notes.reset();
+
+    expect(notes.getNotes()).toEqual([]);
+  })
 });
